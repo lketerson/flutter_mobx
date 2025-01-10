b8e925041fcd0998e4073aa0ac6be66669b9efc4
lketerson <lucasketerson@gmail.com>
2025-01-10 11:03:17 -0300

    Modificado: list screen usa list_store

--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 import '../widgets/custom_icon_button.dart';
 import '../widgets/custom_text_field.dart';
 import 'login_screen.dart';
+import 'stores/list_store.dart';
 
 class ListScreen extends StatefulWidget {
   const ListScreen({super.key});
@@ -12,6 +14,8 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
+  final ListStore listStore = ListStore();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -55,31 +59,46 @@ class _ListScreenState extends State<ListScreen> {
                     padding: const EdgeInsets.all(16),
                     child: Column(
                       children: <Widget>[
-                        CustomTextField(
-                          hint: 'Tarefa',
-                          onChanged: (todo) {},
-                          suffix: CustomIconButton(
-                            radius: 32,
-                            iconData: Icons.add,
-                            onTap: () {},
-                          ),
+                        Observer(
+                          builder: (_) {
+                            return CustomTextField(
+                              hint: 'Tarefa',
+                              onChanged: listStore.setTodoTitle,
+                              suffix: Visibility(
+                                visible: listStore.isTextValid,
+                                child: CustomIconButton(
+                                  radius: 32,
+                                  iconData: Icons.add,
+                                  onTap: listStore.addTodo,
+                                ),
+                              ),
+                            );
+                          },
                         ),
                         const SizedBox(
                           height: 8,
                         ),
                         Expanded(
-                          child: ListView.separated(
-                            itemCount: 10,
-                            itemBuilder: (_, index) {
-                              return ListTile(
-                                title: Text(
-                                  'Item $index',
-                                ),
-                                onTap: () {},
+                          child: Observer(
+                            builder: (_) {
+                              return ListView.separated(
+                                itemCount: listStore.todoList.length,
+                                itemBuilder: (_, index) {
+                                  return ListTile(
+                                    title: Text(
+                                      'Todo: ${listStore.todoList[index]}',
+                                    ),
+                                    onTap: () {},
+                                    trailing: IconButton(
+                                        onPressed: () =>
+                                            listStore.removeTodo(index),
+                                        icon: const Icon(Icons.delete)),
+                                  );
+                                },
+                                separatorBuilder: (_, __) {
+                                  return const Divider();
+                                },
                               );
-                            },
-                            separatorBuilder: (_, __) {
-                              return const Divider();
                             },
                           ),
                         ),
